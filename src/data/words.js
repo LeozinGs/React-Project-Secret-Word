e7dfcd4664c2969171f2b6dd9a9f58fe909529ea
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-07 12:45:40 -0300

    🎉 Começando o projeto Secret Word

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Pedal", "Volante", "Câmbio", "Pneu"],
+    fruta: ["Banana", "Maçã", "Laranja", "Coco", "Pitaya"],
+    flor: ["Caule", "Pétala", "Folha", "Espinho", "Perfume"],
+    marca: ["Starbucks", "Honda", "Pichau", "Corsair", "Razer"],
+    valorant: ["Jett", "Omen", "Viper", "Killjoy", "Raze"],
+    programação: ["React", "JavaScript", "Java", "Unreal", "Unity"]
+}

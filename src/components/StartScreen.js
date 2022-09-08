1c289748bcd243db5b672987320d709edbea150b
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-08 01:32:57 -0300

    ✨ Aprimora o design da primeira pagina

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -3,26 +3,9 @@ import './StartScreen.css'
 const StartScreen = () => {
   return (
     <div className="start">
-        <h1>Secret Word</h1>
-        <p>Clique no Botão para começar a jogar!</p>
-        <button>Start Game</button>
-
-        {/* Circulos animados */}
-        <div className="area" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    </div >
-
+      <h1>Secret Word</h1>
+      <p>Clique no Botão para começar a jogar!</p>
+      <button>Start Game</button>
     </div>
   )
 }

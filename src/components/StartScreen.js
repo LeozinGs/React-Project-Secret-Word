f28177f6a2ba8f7c0c07f8dfb6e16f8a681408cb
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-07 23:26:11 -0300

    ✨ Adcionando background animado

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,6 +6,23 @@ const StartScreen = () => {
         <h1>Secret Word</h1>
         <p>Clique no Botão para começar a jogar!</p>
         <button>Start Game</button>
+
+        {/* Circulos animados */}
+        <div className="area" >
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+
     </div>
   )
 }

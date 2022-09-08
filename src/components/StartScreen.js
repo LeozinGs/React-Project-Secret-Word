ee9c34743a9dd5aab9b4cbfeb5fdb59570b72658
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-08 15:05:19 -0300

    ✅ Muda todo o estilo do StartScreen com SASS

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,22 @@
-import './StartScreen.css'
+import './StartScreen.sass'
 
 const StartScreen = () => {
   return (
     <div className="start">
-      <h1>Secret Word</h1>
+      {/* <h1>Secret Word</h1> */}
+      <div class="text">
+        <span>S</span>
+        <span>E</span>
+        <span>C</span>
+        <span>R</span>
+        <span>E</span>
+        <span>T</span>
+        <span></span>
+        <span>W</span>
+        <span>O</span>
+        <span>R</span>
+        <span>D</span>
+      </div>
       <p>Clique no Botão para começar a jogar!</p>
       <button>Start Game</button>
     </div>

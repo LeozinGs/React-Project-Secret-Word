6ef061eedf6f832ef80c8b37e00f98fcd7940c5f
Leoziin <lokinho_ddtank@hotmail.com>
2022-09-09 19:04:05 -0300

    💄 Aumenta a caixa que segura as letras

--- src/data/words.js
+++ src/data/words.js
@@ -1,9 +1,9 @@
 export const wordsList = {
-    carro: ["Motor", "Pedal", "Volante", "Câmbio", "Pneu"],
-    fruta: ["Banana", "Maçã", "Laranja", "Coco", "Pitaya"],
-    flor: ["Caule", "Pétala", "Folha", "Espinho", "Perfume"],
-    marca: ["Starbucks", "Honda", "Pichau", "Corsair", "Razer"],
-    valorant: ["Jett", "Omen", "Viper", "Killjoy", "Raze"],
-    programação: ["React", "JavaScript", "Java", "Unreal", "Unity"],
-    discord: ["Rurushu", "Mizu", "Minkas", "Aroldu", "Greiso", "Venes", "Leozinho", "Ordnave"]
+    Carro: ["Motor", "Pedal", "Volante", "Câmbio", "Pneu"],
+    Frutas: ["Banana", "Maçã", "Laranja", "Coco", "Pitaya"],
+    Flor: ["Caule", "Pétala", "Folha", "Espinho", "Perfume"],
+    Marcas: ["Starbucks", "Honda", "Pichau", "Corsair", "Razer"],
+    Valorant: ["Jett", "Omen", "Viper", "Killjoy", "Raze"],
+    Programação: ["React", "JavaScript", "Java", "Unreal", "Unity"],
+    Discord: ["Rurushu", "Mizu", "Minkas", "Aroldu", "Greiso", "Venes", "Leozinho", "Ordnave"]
 }
